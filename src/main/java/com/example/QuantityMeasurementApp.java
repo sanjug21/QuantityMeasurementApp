51b45e3d2d823e191d51109fa759825ae2c702ea
Sanju <psinghrajput907@gmail.com>
2026-02-18 20:33:38 +0530

    [ Pushpendra Singh ] feat. add UC 2: Feat and Inches Measurement Equality

--- src/main/java/com/example/QuantityMeasurementApp.java
+++ src/main/java/com/example/QuantityMeasurementApp.java
@@ -29,14 +29,66 @@ public class QuantityMeasurementApp
         }
     }
 
-    public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
-        Feet feet3 = new Feet(2.0);
+    public static class Inches {
+        private final double value;
 
-        System.out.println("Comparing 1.0 ft and 1.0 ft: " + feet1.equals(feet2));
-        System.out.println("Comparing 1.0 ft and 2.0 ft: " + feet1.equals(feet3));
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    // Static method for Feet equality check
+    public static boolean checkFeetEquality(double value1, double value2) {
+        Feet feet1 = new Feet(value1);
+        Feet feet2 = new Feet(value2);
+        return feet1.equals(feet2);
+    }
+
+    // Static method for Inches equality check
+    public static boolean checkInchesEquality(double value1, double value2) {
+        Inches inches1 = new Inches(value1);
+        Inches inches2 = new Inches(value2);
+        return inches1.equals(inches2);
+    }
+
+    public static void main(String[] args) {
+        // Using static methods for Feet equality checks
+        System.out.println("=== Feet Comparisons ===");
+        System.out.println("Comparing 1.0 ft and 1.0 ft: " + checkFeetEquality(1.0, 1.0));
+        System.out.println("Comparing 1.0 ft and 2.0 ft: " + checkFeetEquality(1.0, 2.0));
+        
+        // Direct object comparison for additional cases
+        Feet feet1 = new Feet(1.0);
         System.out.println("Comparing 1.0 ft with null: " + feet1.equals(null));
         System.out.println("Comparing 1.0 ft with itself: " + feet1.equals(feet1));
+        
+        // Using static methods for Inches equality checks
+        System.out.println("\n=== Inches Comparisons ===");
+        System.out.println("Comparing 1.0 inch and 1.0 inch: " + checkInchesEquality(1.0, 1.0));
+        System.out.println("Comparing 1.0 inch and 2.0 inch: " + checkInchesEquality(1.0, 2.0));
+        
+        // Direct object comparison for additional cases
+        Inches inches1 = new Inches(1.0);
+        System.out.println("Comparing 1.0 inch with null: " + inches1.equals(null));
+        System.out.println("Comparing 1.0 inch with itself: " + inches1.equals(inches1));
     }
 }
--- src/test/java/com/example/QuantityMeasurementAppTest.java
+++ src/test/java/com/example/QuantityMeasurementAppTest.java
@@ -99,4 +99,131 @@ public class QuantityMeasurementAppTest
         QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.5);
         assertEquals(feet1, feet2, "1.5 ft should be equal to 1.5 ft");
     }
+
+    // === INCHES EQUALITY TEST CASES ===
+
+    @Test
+    @DisplayName("Given two inches measurements of 1.0, when compared, then should be equal")
+    public void testInchesEquality_SameValue()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(1.0);
+        assertEquals(inches1, inches2, "The measurements should be identical");
+    }
+
+    @Test
+    @DisplayName("Given two inches measurements of 1.0 and 2.0, when compared, then should not be equal")
+    public void testInchesEquality_DifferentValue()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(2.0);
+        assertNotEquals(inches1, inches2, "1.0 inch should not be equal to 2.0 inch");
+    }
+
+    @Test
+    @DisplayName("Given an inches measurement, when compared to null, then should not be equal")
+    public void testInchesEquality_NullComparison()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        assertNotEquals(inches1, null, "Inches object should not be equal to null");
+    }
+
+    @Test
+    @DisplayName("Given an inches measurement, when compared to non-Inches object, then should not be equal")
+    public void testInchesEquality_NonNumericInput()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        String nonNumeric = "1.0";
+        assertNotEquals(inches1, nonNumeric, "Inches object should not be equal to non-Inches object");
+    }
+
+    @Test
+    @DisplayName("Given an inches measurement, when compared to itself, then should be equal")
+    public void testInchesEquality_SameReference()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        assertEquals(inches1, inches1, "Inches object should be equal to itself (reflexive)");
+    }
+
+    @Test
+    @DisplayName("Given two equal inches measurements, when compared in any order, then should be symmetric")
+    public void testInchesEquality_SymmetricProperty()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(3.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(3.0);
+        assertEquals(inches1, inches2, "Equality should be symmetric");
+        assertEquals(inches2, inches1, "Equality should be symmetric");
+    }
+
+    @Test
+    @DisplayName("Given three equal inches measurements, when compared transitively, then should be equal")
+    public void testInchesEquality_TransitiveProperty()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(5.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(5.0);
+        QuantityMeasurementApp.Inches inches3 = new QuantityMeasurementApp.Inches(5.0);
+        assertEquals(inches1, inches2, "Equality should be transitive");
+        assertEquals(inches2, inches3, "Equality should be transitive");
+        assertEquals(inches1, inches3, "Equality should be transitive");
+    }
+
+    @Test
+    @DisplayName("Given two inches measurements of 0.0, when compared, then should be equal")
+    public void testInchesEquality_ZeroValue()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(0.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(0.0);
+        assertEquals(inches1, inches2, "0.0 inch should be equal to 0.0 inch");
+    }
+
+    @Test
+    @DisplayName("Given two inches measurements of -1.0, when compared, then should be equal")
+    public void testInchesEquality_NegativeValue()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(-1.0);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(-1.0);
+        assertEquals(inches1, inches2, "-1.0 inch should be equal to -1.0 inch");
+    }
+
+    @Test
+    @DisplayName("Given two inches measurements with decimal values, when compared, then should handle precision")
+    public void testInchesEquality_DecimalPrecision()
+    {
+        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.5);
+        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(1.5);
+        assertEquals(inches1, inches2, "1.5 inch should be equal to 1.5 inch");
+    }
+
+    @Test
+    @DisplayName("Given a Feet object and an Inches object, when compared, then should not be equal")
+    public void testInchesEquality_DifferentTypes()
+    {
+        QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Inches inches = new QuantityMeasurementApp.Inches(1.0);
+        assertNotEquals(feet, inches, "Feet and Inches objects should not be equal (different types)");
+    }
+
+    // === STATIC METHOD TEST CASES ===
+
+    @Test
+    @DisplayName("Given two feet values using static method, when compared, then should return correct equality")
+    public void testStaticMethod_FeetEquality()
+    {
+        boolean result1 = QuantityMeasurementApp.checkFeetEquality(1.0, 1.0);
+        boolean result2 = QuantityMeasurementApp.checkFeetEquality(1.0, 2.0);
+        
+        assertEquals(true, result1, "1.0 ft should be equal to 1.0 ft");
+        assertEquals(false, result2, "1.0 ft should not be equal to 2.0 ft");
+    }
+
+    @Test
+    @DisplayName("Given two inches values using static method, when compared, then should return correct equality")
+    public void testStaticMethod_InchesEquality()
+    {
+        boolean result1 = QuantityMeasurementApp.checkInchesEquality(1.0, 1.0);
+        boolean result2 = QuantityMeasurementApp.checkInchesEquality(1.0, 2.0);
+        
+        assertEquals(true, result1, "1.0 inch should be equal to 1.0 inch");
+        assertEquals(false, result2, "1.0 inch should not be equal to 2.0 inch");
+    }
 }
